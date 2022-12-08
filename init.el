eb34ce63e476a6d7bf8a0478a93940c132bcf7fe
Shammyshanks <thorinbriggz@gmail.com>
2022-12-08 14:56:49 -0800

    Add files via upload

---
+++ init.el
@@ -0,0 +1,29 @@
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+(setq warning-minimum-level :emergency)
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-journal which-key elfeed smex avy rainbow-mode doom-modeline all-the-icons rainbow-delimiters catppuccin-theme gruvbox-theme ido-vertical-mode lua-mode nix-mode dashboard use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
